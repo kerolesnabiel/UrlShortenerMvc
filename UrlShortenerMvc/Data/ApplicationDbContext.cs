e67724d8a4366b44d75ebd35e141238708b6ea72
kerolesnabiel <keronabilzaki@gmail.com>
2026-09-05 11:01:45 +0300

    Add project files.

---
+++ UrlShortenerMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UrlShortenerMvc.Models;
+
+namespace UrlShortenerMvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ UrlShortenerMvc/Models/User.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace UrlShortenerMvc.Models;
+
+public class User : IdentityUser<Guid>
+{
+
+}
